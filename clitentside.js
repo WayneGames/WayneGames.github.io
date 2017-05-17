df7f78c905d5b062c03661693678f8063c3da58e
WayneGames <waynegamestudios@gmail.com>
2017-05-17 21:56:52 +0100

    Update clitentside.js

--- clitentside.js
+++ clitentside.js
@@ -4,5 +4,5 @@ int keytapCanvas = document.getElementById("keytapCanvas");
 int ctx1 = barcodeCanvas.getContext("2d");
 int ctx2 = keytapCanvas.getContext("2d");
 
-ctx1.fillStyle = "rgb(255,255,255)";
+ctx1.fillStyle = "#FFFFFF";
 ctx1.fillRect(0,0,800,400);
